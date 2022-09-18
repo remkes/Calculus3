0782fd977e9557c64271cc67537cc0de4c927019
remkes <remkes.kooistra@kingsu.ca>
2022-09-17 23:51:22 -0600

    work on images

---
+++ asy/processasyhtml.sh
@@ -0,0 +1,77 @@
+#!/bin/zsh
+
+# Process images
+asy -f html figure01.asy
+asy -f html figure02.asy
+asy -f html figure04.asy
+asy -f html figure05.asy
+asy -f html figure06.asy
+asy -f html figure08.asy
+asy -f html figure10.asy
+asy -f html figure11.asy
+asy -f html figure12.asy
+asy -f html figure13.asy
+asy -f html figure14.asy
+asy -f html figure15.asy
+asy -f html figure16.asy
+asy -f html figure17.asy
+asy -f html figure18.asy
+asy -f html figure19.asy
+asy -f html figure20.asy
+asy -f html figure21.asy
+asy -f html figure22.asy
+asy -f html figure23.asy
+asy -f html figure24.asy
+asy -f html figure25.asy
+asy -f html figure26.asy
+asy -f html figure27.asy
+asy -f html figure28.asy
+asy -f html figure29.asy
+asy -f html figure30.asy
+asy -f html figure32.asy
+asy -f html figure33.asy
+asy -f html figure34.asy
+asy -f html figure35.asy
+asy -f html figure36.asy
+asy -f html figure37.asy
+asy -f html figure38.asy
+asy -f html figure39.asy
+asy -f html figure40.asy
+asy -f html figure41.asy
+asy -f html figure43.asy
+asy -f html figure44.asy
+asy -f html figure45.asy
+asy -f html figure46.asy
+asy -f html figure47.asy
+asy -f html figure48.asy
+asy -f html figure49.asy
+asy -f html figure50.asy
+asy -f html figure51.asy
+asy -f html figure52.asy
+asy -f html figure53.asy
+asy -f html figure54.asy
+asy -f html figure55.asy
+asy -f html figure56.asy
+asy -f html figure57.asy
+asy -f html figure58.asy
+asy -f html figure59.asy
+asy -f html figure60.asy
+asy -f html figure61.asy
+asy -f html figure62.asy
+asy -f html figure63.asy
+asy -f html figure64.asy
+asy -f html figure65.asy
+asy -f html figure66.asy
+asy -f html figure67.asy
+asy -f html figure68.asy
+asy -f html figure69.asy
+asy -f html figure70.asy
+asy -f html figure71.asy
+asy -f html figure72.asy
+asy -f html figure73.asy
+asy -f html figure74.asy
+asy -f html figure75.asy
+
+# Copy and move files
+cp *.html ../images/
+mv *.html ../html/images
