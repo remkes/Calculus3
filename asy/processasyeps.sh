7f7f0e6209e0348dd81aec0e581c95d108b7403a
remkes <remkes.kooistra@kingsu.ca>
2022-09-19 10:47:36 -0600

    few typos

---
+++ asy/processasyeps.sh
@@ -0,0 +1,77 @@
+#!/bin/zsh
+
+# Process images
+#
+asy -f eps figure01.asy
+asy -f eps figure02.asy
+asy -f eps figure04.asy
+asy -f eps figure05.asy
+asy -f eps figure06.asy
+asy -f eps figure08.asy
+asy -f eps figure10.asy
+asy -f eps figure11.asy
+asy -f eps figure12.asy
+asy -f eps figure13.asy
+asy -f eps figure14.asy
+asy -f eps figure15.asy
+asy -f eps figure16.asy
+asy -f eps figure17.asy
+asy -f eps figure18.asy
+asy -f eps figure19.asy
+asy -f eps figure20.asy
+asy -f eps figure21.asy
+asy -f eps figure22.asy
+asy -f eps figure23.asy
+asy -f eps figure24.asy
+asy -f eps figure25.asy
+asy -f eps figure26.asy
+asy -f eps figure27.asy
+asy -f eps figure28.asy
+asy -f eps figure29.asy
+asy -f eps figure30.asy
+asy -f eps figure32.asy
+asy -f eps figure33.asy
+asy -f eps figure34.asy
+asy -f eps figure35.asy
+asy -f eps figure36.asy
+asy -f eps figure37.asy
+asy -f eps figure38.asy
+asy -f eps figure39.asy
+asy -f eps figure40.asy
+asy -f eps figure41.asy
+asy -f eps figure43.asy
+asy -f eps figure44.asy
+asy -f eps figure45.asy
+asy -f eps figure46.asy
+asy -f eps figure47.asy
+asy -f eps figure48.asy
+asy -f eps figure49.asy
+asy -f eps figure50.asy
+asy -f eps figure51.asy
+asy -f eps figure52.asy
+asy -f eps figure53.asy
+asy -f eps figure54.asy
+asy -f eps figure55.asy
+asy -f eps figure56.asy
+asy -f eps figure57.asy
+asy -f eps figure58.asy
+asy -f eps figure59.asy
+asy -f eps figure60.asy
+asy -f eps figure61.asy
+asy -f eps figure62.asy
+asy -f eps figure63.asy
+asy -f eps figure64.asy
+asy -f eps figure65.asy
+asy -f eps figure66.asy
+asy -f eps figure67.asy
+asy -f eps figure68.asy
+asy -f eps figure69.asy
+asy -f eps figure70.asy
+asy -f eps figure71.asy
+asy -f eps figure72.asy
+asy -f eps figure73.asy
+asy -f eps figure74.asy
+asy -f eps figure75.asy
+
+# Copy and move files
+mv *.eps ../images/
